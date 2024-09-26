445803edd873c592a253192d399accd0b1496662
Lio <liobensignor@gmail.com>
2024-09-26 20:29:45 -0300

    Fuente + Logo + Estilos Css generales

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -20,18 +20,18 @@ export default defineNuxtConfig({
           rel: 'stylesheet',
           href: 'https://unpkg.com/primeflex@latest/primeflex.css'
         },
-        // {
-        //   rel: 'preconnect',
-        //   href: 'https://fonts.googleapis.com'
-        // },
-        // {
-        //   rel: 'preconnect',
-        //   href: 'https://fonts.gstatic.com'
-        // },
-        // {
-        //   rel: 'stylesheet',
-        //   href: 'https://fonts.googleapis.com/css2?family=Figtree:ital,wght@0,300..900;1,300..900&display=swap'
-        // },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.googleapis.com'
+        },
+        {
+          rel: 'preconnect',
+          href: 'https://fonts.gstatic.com'
+        },
+        {
+          rel: 'stylesheet',
+          href: 'https://fonts.googleapis.com/css2?family=Outfit:wght@100..900&display=swap'
+        },
       ],
     }
   },
