1a0efc81f8ca0a4baceb0a62c0302b85d378ec52
Emilio Bensignor <liobensignor@gmail.com>
2024-09-29 19:01:43 -0300

    Maquetacion para Swiper

--- components/home/Projects.vue
+++ components/home/Projects.vue
@@ -1,13 +1,92 @@
 <template>
   <section class="w-full projects columnAlignCenter">
-    <h2>
-      Our most <span class="text-lime underline">recent work.</span>
-    </h2>
+    <h2>Our most <span class="text-lime underline">recent work.</span></h2>
+    <div class="swiper swiperProjects">
+      <div class="swiper-wrapper">
+        <div
+          class="swiper-slide"
+          v-for="(project, index) in projects"
+          :key="index">
+          <NuxtImg
+            :src="`/images/home/${project.img}.png`"
+            :alt="project.alt" />
+          <h3>{{ project.title }}</h3>
+          <p>{{ project.service }}</p>
+        </div>
+      </div>
+    </div>
   </section>
 </template>
 
+<script>
+  import { Swiper } from "swiper";
+  import "swiper/css";
+
+  export default {
+    data() {
+      return {
+        projects: [
+          {
+            img: "projectExample",
+            alt: "Project Example",
+            title: "Stock Go",
+            service: "Hardware and Software Design",
+          },
+          {
+            img: "projectExample",
+            alt: "Project Example",
+            title: "Stock Go",
+            service: "Hardware and Software Design",
+          },
+          {
+            img: "projectExample",
+            alt: "Project Example",
+            title: "Stock Go",
+            service: "Hardware and Software Design",
+          },
+          {
+            img: "projectExample",
+            alt: "Project Example",
+            title: "Stock Go",
+            service: "Hardware and Software Design",
+          },
+          {
+            img: "projectExample",
+            alt: "Project Example",
+            title: "Stock Go",
+            service: "Hardware and Software Design",
+          },
+          {
+            img: "projectExample",
+            alt: "Project Example",
+            title: "Stock Go",
+            service: "Hardware and Software Design",
+          },
+        ],
+      };
+    },
+    mounted() {
+      this.$nextTick(() => {
+        this.initSwiper();
+      });
+    },
+    methods: {
+      initSwiper() {
+        new Swiper(".swiperProjects", {
+          slidesPerView: "auto",
+          spaceBetween: 12,
+        });
+      },
+    },
+  };
+</script>
+
 <style scoped>
-.projects {
-  padding: 2rem 0 2rem 1rem;
-}
+  .projects {
+    padding: 2rem 0 2rem 1rem;
+  }
+
+  .swiper-slide {
+    width: 300px; /* Ajusta según tus necesidades */
+  }
 </style>
