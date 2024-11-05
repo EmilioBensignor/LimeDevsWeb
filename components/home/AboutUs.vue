dbdc420f0b64c659e026c56a07450f558e34b563
Lio <liobensignor@gmail.com>
2024-11-04 22:44:45 -0300

    Test v5 bg about

--- components/home/AboutUs.vue
+++ components/home/AboutUs.vue
@@ -15,95 +15,97 @@
         </p>
       </div>
     </div>
-    <div
-      v-if="isMobile"
-      class="w-full accordionMobile pb-5"
-      ref="mobileSection"
-    >
-      <Accordion :value="0" class="w-full">
-        <AccordionPanel
-          :value="value.value"
-          v-for="(value, index) in values"
-          :key="index"
-          @click="handleAccordionClick(index)"
-        >
-          <AccordionHeader :class="value.class">
-            <h3>{{ value.title }}</h3>
-          </AccordionHeader>
-          <AccordionContent>
-            <div class="columnAlignCenter">
-              <div class="animationAbout">
-                <video
-                  ref="mobileVideos"
-                  autoplay
-                  muted
-                  playsinline
-                  preload="auto"
-                  poster="/images/home/Lime-Devs-Values.webp"
-                  width="100%"
-                  height="auto"
-                >
-                  <source
-                    :src="`/videos/Lime-Devs-${value.title}-Product.webm`"
-                    type="video/webm"
-                  />
-                  Tu navegador no soporta el video.
-                </video>
-              </div>
-              <p v-html="value.text" class="text-center"></p>
-            </div>
-          </AccordionContent>
-        </AccordionPanel>
-      </Accordion>
-    </div>
-    <div v-else class="aboutDesktop">
-      <div class="aboutContainer column">
-        <div
-          class="atLimeDesktop bg-gradient-light-violet"
-          :class="values[selectedValue].border"
-        >
-          <p>
-            At Lime, we are a team with
-            <span class="text-lime font-bold"
-              >expertise in both digital and physical products</span
-            >, focused on solving the concrete, day-to-day challenges of your
-            business.
-          </p>
-        </div>
-        <div class="column">
-          <div
-            v-for="(item, index) in values"
+    <div class="background-overlay">
+      <div
+        v-if="isMobile"
+        class="w-full accordionMobile pb-5"
+        ref="mobileSection"
+      >
+        <Accordion :value="0" class="w-full">
+          <AccordionPanel
+            :value="value.value"
+            v-for="(value, index) in values"
             :key="index"
-            @click="setSelectedStep(index)"
-            :class="{ valueBtnActive: selectedValue === index }"
-            class="valueBtn relative bg-blue-light-gradient"
+            @click="handleAccordionClick(index)"
           >
-            <h3>{{ item.title }}</h3>
+            <AccordionHeader :class="value.class">
+              <h3>{{ value.title }}</h3>
+            </AccordionHeader>
+            <AccordionContent>
+              <div class="columnAlignCenter">
+                <div class="animationAbout">
+                  <video
+                    ref="mobileVideos"
+                    autoplay
+                    muted
+                    playsinline
+                    preload="auto"
+                    poster="/images/home/Lime-Devs-Values.webp"
+                    width="100%"
+                    height="auto"
+                  >
+                    <source
+                      :src="`/videos/Lime-Devs-${value.title}-Product.webm`"
+                      type="video/webm"
+                    />
+                    Tu navegador no soporta el video.
+                  </video>
+                </div>
+                <p v-html="value.text" class="text-center"></p>
+              </div>
+            </AccordionContent>
+          </AccordionPanel>
+        </Accordion>
+      </div>
+      <div v-else class="aboutDesktop">
+        <div class="aboutContainer column">
+          <div
+            class="atLimeDesktop bg-gradient-light-violet"
+            :class="values[selectedValue].border"
+          >
+            <p>
+              At Lime, we are a team with
+              <span class="text-lime font-bold"
+                >expertise in both digital and physical products</span
+              >, focused on solving the concrete, day-to-day challenges of your
+              business.
+            </p>
+          </div>
+          <div class="column">
+            <div
+              v-for="(item, index) in values"
+              :key="index"
+              @click="setSelectedStep(index)"
+              :class="{ valueBtnActive: selectedValue === index }"
+              class="valueBtn relative bg-blue-light-gradient"
+            >
+              <h3>{{ item.title }}</h3>
+            </div>
           </div>
         </div>
-      </div>
-      <div class="desktopContentPanel column">
-        <div class="columnAlignCenter">
-          <div ref="animationContainer" class="animationAbout">
-            <video
-              :key="`desktop-video-${selectedValue}`"
-              ref="desktopVideo"
-              autoplay
-              muted
-              playsinline
-              preload="auto"
-              poster="/images/home/Lime-Devs-Values.webp"
-              width="100%"
-              height="auto"
-            >
-              <source
-                :src="`/videos/Lime-Devs-${values[selectedValue].title}-Product.webm`"
-                type="video/webm"
-              />
-              Tu navegador no soporta el video.
-            </video>
+        <div class="desktopContentPanel column">
+          <div class="columnAlignCenter">
+            <div ref="animationContainer" class="animationAbout">
+              <video
+                :key="`desktop-video-${selectedValue}`"
+                ref="desktopVideo"
+                autoplay
+                muted
+                playsinline
+                preload="auto"
+                poster="/images/home/Lime-Devs-Values.webp"
+                width="100%"
+                height="auto"
+              >
+                <source
+                  :src="`/videos/Lime-Devs-${values[selectedValue].title}-Product.webm`"
+                  type="video/webm"
+                />
+                Tu navegador no soporta el video.
+              </video>
+            </div>
+            <p v-html="values[selectedValue].text" class="text-center"></p>
           </div>
-          <p v-html="values[selectedValue].text" class="text-center"></p>
         </div>
       </div>
     </div>
@@ -415,9 +417,12 @@ export default {
 
 <style scoped>
 .aboutUs {
-  background: transparent !important;
   position: relative;
   z-index: 1;
+}
+
+.background-overlay {
+  background-color: var(--color-dark-violet);
 }
 
 .aboutUs > div:first-of-type {
@@ -451,8 +456,12 @@ video {
 }
 
 @supports (-webkit-touch-callout: none) {
+  .background-overlay {
+    background-color: var(--color-dark-violet);
+  }
+
   .aboutUs {
-    background: none !important;
+    isolation: isolate;
   }
 }
 
